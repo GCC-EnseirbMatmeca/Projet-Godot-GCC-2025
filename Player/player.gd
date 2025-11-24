28f2e25788c8b549349ae2a9eee4eefd3546cf11
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2025-11-24 19:17:08 +0100

    init

---
+++ Player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 300.0 * 60
+
+func _ready() -> void :
+	pass
+
+func _physics_process(delta): # delta = 1/fps	
+	var direction : Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	if direction:
+		velocity = direction * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
